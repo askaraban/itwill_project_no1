60f237086b278ead47c0c11c1e6a5968ee2005cb
itwill <itwill@DESKTOP-UCB2PUT>
2023-12-13 17:20:54 +0900

    UI 입니다.

---
+++ projectOne/src/itwill/xyz/jdcbtam1/PracticeUi.java
@@ -0,0 +1,163 @@
+package itwill.xyz.jdcbtam1;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import java.awt.FlowLayout;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import javax.swing.JSplitPane;
+import javax.swing.JTable;
+import javax.swing.BoxLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.Component;
+import java.awt.GridLayout;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.table.DefaultTableModel;
+import java.awt.Panel;
+import java.awt.SystemColor;
+import java.awt.Font;
+import javax.swing.JTabbedPane;
+import javax.swing.JLayeredPane;
+import javax.swing.JDesktopPane;
+import java.awt.CardLayout;
+import javax.swing.Box;
+import javax.swing.SpringLayout;
+import javax.swing.JSpinner;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.Button;
+
+public class PracticeUi {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PracticeUi window = new PracticeUi();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public PracticeUi() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_3 = new JPanel();
+		frame.getContentPane().add(panel_3, BorderLayout.SOUTH);
+		panel_3.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JButton btnNewButton_2 = new JButton("입금");
+		panel_3.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("출금");
+		panel_3.add(btnNewButton_3);
+		
+		textField = new JTextField();
+		panel_3.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("~");
+		panel_3.add(lblNewLabel);
+		
+		textField_1 = new JTextField();
+		panel_3.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"입금", "출금"}));
+		comboBox.setMaximumRowCount(2);
+		panel_3.add(comboBox);
+		
+		JButton btnNewButton_4 = new JButton("조회");
+		panel_3.add(btnNewButton_4);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"New column", "New column", "New column", "New column", "New column"
+			}
+		));
+		scrollPane.setViewportView(table);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.NORTH);
+		panel.setLayout(new GridLayout(2, 4, 0, 0));
+		
+		Panel panel_1 = new Panel();
+		panel.add(panel_1);
+		panel_1.setLayout(new GridLayout(0, 6, 0, 0));
+		
+		JPanel panel_2 = new JPanel();
+		panel_1.add(panel_2);
+		panel_2.setLayout(new GridLayout(2, 1, 0, 0));
+		
+		JButton btnNewButton = new JButton("로그인");
+		panel_2.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("로그아웃");
+		panel_2.add(btnNewButton_1);
+		
+		JPanel panel_7 = new JPanel();
+		panel_1.add(panel_7);
+		panel_7.setLayout(new GridLayout(2, 1, 0, 0));
+		
+		JButton btnNewButton_6 = new JButton("회원가입");
+		btnNewButton_6.setToolTipText("회원탈퇴");
+		panel_7.add(btnNewButton_6);
+		
+		JButton btnNewButton_7 = new JButton("회원탈퇴");
+		panel_7.add(btnNewButton_7);
+		
+		JPanel panel_8 = new JPanel();
+		panel_1.add(panel_8);
+		panel_8.setLayout(new GridLayout(1, 2, 0, 0));
+		
+		JButton btnNewButton_5 = new JButton("내 정보");
+		panel_8.add(btnNewButton_5);
+		
+		JPanel panel_4 = new JPanel();
+		panel_1.add(panel_4);
+		
+		JPanel panel_5 = new JPanel();
+		panel_1.add(panel_5);
+		
+		JPanel panel_6 = new JPanel();
+		panel_1.add(panel_6);
+	}
+}
