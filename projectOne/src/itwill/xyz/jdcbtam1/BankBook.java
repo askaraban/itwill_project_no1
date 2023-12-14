add32928127d91d98346299527db2bea1d43a766
itwill <itwill@DESKTOP-9E0SHOP>
2023-12-14 15:46:01 +0900

    DTO

---
+++ projectOne/src/itwill/xyz/jdcbtam1/BankBook.java
@@ -0,0 +1,105 @@
+package itwill.xyz.jdcbtam1;
+
+public class BankBook {
+	private String ID;
+	private int InOutType;
+	private long InputMoney;
+	private long OutputMoney;
+	private String Date;
+	private String Memo;
+	private long LastMoney;
+	
+	
+	public BankBook() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public BankBook(String iD, int inOutType, long inputMoney, long outputMoney, String date, String memo,
+			long lastMoney) {
+		super();
+		ID = iD;
+		InOutType = inOutType;
+		InputMoney = inputMoney;
+		OutputMoney = outputMoney;
+		Date = date;
+		Memo = memo;
+		LastMoney = lastMoney;
+	}
+
+
+	public String getID() {
+		return ID;
+	}
+
+
+	public void setID(String iD) {
+		ID = iD;
+	}
+
+
+	public int getInOutType() {
+		return InOutType;
+	}
+
+
+	public void setInOutType(int inOutType) {
+		InOutType = inOutType;
+	}
+
+
+	public long getInputMoney() {
+		return InputMoney;
+	}
+
+
+	public void setInputMoney(long inputMoney) {
+		InputMoney = inputMoney;
+	}
+
+
+	public long getOutputMoney() {
+		return OutputMoney;
+	}
+
+
+	public void setOutputMoney(long outputMoney) {
+		OutputMoney = outputMoney;
+	}
+
+
+	public String getDate() {
+		return Date;
+	}
+
+
+	public void setDate(String date) {
+		Date = date;
+	}
+
+
+	public String getMemo() {
+		return Memo;
+	}
+
+
+	public void setMemo(String memo) {
+		Memo = memo;
+	}
+
+
+	public long getLastMoney() {
+		return LastMoney;
+	}
+
+
+	public void setLastMoney(long lastMoney) {
+		LastMoney = lastMoney;
+	}
+
+
+	
+	
+	
+	
+}
--- projectOne/src/itwill/xyz/jdcbtam1/PracticeUi.java
+++ projectOne/src/itwill/xyz/jdcbtam1/PracticeUi.java
@@ -5,52 +5,48 @@ import java.awt.EventQueue;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Vector;
 
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 
-public class PracticeUi {
-
+public class PracticeUi extends JFrame {
+	private static final long serialVersionUID = 1L;
+	
+	
+	
 	private JFrame frame;
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTable table;
+	private JButton InBtn;
+	private InputMoney inin;
+	
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					PracticeUi window = new PracticeUi();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	/**
 	 * Create the application.ㅇ
 	 */ 
-	public PracticeUi() {
-		initialize(); 
+	public PracticeUi(String title) {
+		initialize(title); 
 	} 
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {
+	private void initialize(String title) {
 		
 		frame = new JFrame();
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
@@ -60,8 +56,8 @@ public class PracticeUi {
 		frame.getContentPane().add(panel_3, BorderLayout.SOUTH);
 		panel_3.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		JButton btnNewButton_2 = new JButton("입금");
-		panel_3.add(btnNewButton_2);
+		InBtn = new JButton("입금");
+		panel_3.add(InBtn);
 		
 		JButton btnNewButton_3 = new JButton("출금");
 		panel_3.add(btnNewButton_3);
@@ -153,5 +149,110 @@ public class PracticeUi {
 		
 		JPanel panel_9 = new JPanel();
 		panel_8.add(panel_9);
+		
+		inin = new InputMoney(this, "입금창");
 	}
-}
+	
+	public class InOutMoney implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			Object es = e.getSource();
+			
+			if(es == InBtn) {
+				inin.setVisible(true);
+			}
+			
+		}
+		
+		
+		
+	}
+	
+	public class InputMoney extends JDialog{
+		private static final long serialVersionUID = 1L;
+		
+		JTextField inTF, psTF;
+		JButton okBtn, cancelBtn;
+		
+		public InputMoney(JFrame frame, String title) {
+			super(frame, title, true);
+			
+			setTitle(title);
+			
+			getContentPane().setLayout(new GridLayout(2, 1));
+			
+			JPanel panelOne = new JPanel(new GridLayout(2,2));
+			
+			panelOne.setBorder(new EmptyBorder(10,10,10,10));
+			
+			inTF = new JTextField();
+			psTF = new JTextField();
+			
+			panelOne.add(new JLabel("입금액", JLabel.CENTER));
+			panelOne.add(inTF);
+			panelOne.add(new JLabel("비밀번호", JLabel.CENTER));
+			panelOne.add(psTF);
+			
+			inTF.requestFocus();
+			
+			okBtn = new JButton("입금");
+			cancelBtn = new JButton("취소");
+			
+			JPanel panel2 = new JPanel();
+			panel2.add(okBtn);
+			panel2.add(cancelBtn);
+			
+			getContentPane().add(panelOne);
+			getContentPane().add(panel2);
+			
+			okBtn.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					String in = inTF.getText();
+					String passwd = psTF.getText();
+					
+					Vector<String> vector = new Vector<String>();
+					vector.add(in);
+					vector.add(passwd);
+					
+				DefaultTableModel tableModel = (DefaultTableModel)table.getModel();
+				
+				tableModel.addRow(vector);
+
+				inTF.setText("");
+				psTF.setText("");
+				setVisible(true);
+
+				}
+			});
+			
+				
+			};
+			
+		}
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PracticeUi window = new PracticeUi("계좌관리");
+					window.frame.setVisible(true);
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	
+	
+	}
+	
+	
+
